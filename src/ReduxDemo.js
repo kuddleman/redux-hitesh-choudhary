73a74f56bc0ba0547976d531cf80369d5f02b7b4
Kuddleman <edl1155@gmail.com>
2020-06-01 18:05:55 -0700

    add reduxdemo.js

---
+++ src/ReduxDemo.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react'
+import { createStore } from 'redux'
+
+export default class ReduxDemo extends Component {
+
+  // Step 1 Create Store with reducer and state
+
+
+  render() {
+    return (
+      <Fragment>
+        <p>Hi From Redux</p>
+      </Fragment>
+    )
+  }
+}
