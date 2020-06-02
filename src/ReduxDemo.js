af0f173d60194b7934a6c610814246bc463f87a1
Kuddleman <edl1155@gmail.com>
2020-06-01 18:29:06 -0700

    complete basic redux

--- src/ReduxDemo.js
+++ src/ReduxDemo.js
@@ -1,16 +1,39 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { createStore } from 'redux'
 
 export default class ReduxDemo extends Component {
 
-  // Step 1 Create Store with reducer and state
+  
 
 
   render() {
+
+    // Step 2 Create the reducer which needs state and action
+  const reducer = ( state, action ) => {
+    if( action.type === "ATTACK" ) {
+      return action.payload
+    }if ( action.type === "GREENATTACK" ) {
+      return action.payload
+    }
+    return state;
+}
+
+  // Step 1 Create Store with reducer and state
+  const store = createStore(reducer, "Peace")
+
+  
+  // Step 3 Subscribe
+  store.subscribe(() => {
+    console.log("Store is now", store.getState())
+  })
+
+  //Step 4: Dispatch action
+  store.dispatch({type: "ATTACK", payload: "Iron Man"})
+  store.dispatch({ type: "GREENATTACK", payload: "Green Attack" })
     return (
-      <Fragment>
+      <div>
         <p>Hi From Redux</p>
-      </Fragment>
+      </div>
     )
   }
 }
